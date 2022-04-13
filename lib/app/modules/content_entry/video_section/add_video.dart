518775b4bbf310e8de687a702b3212238879703b
Developerajit <indiandeveloperajit@gmail.com>
2022-04-14 01:19:51 +0530

    updated

--- lib/app/modules/content_entry/video_section/add_video.dart
+++ lib/app/modules/content_entry/video_section/add_video.dart
@@ -1,14 +1,7 @@
-import 'dart:html';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:expandable/expandable.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:management/app/modules/content_entry/upcoming_section/controller/add_upcoming_controller.dart';
 import 'package:management/resources/app_components/custom_widgets.dart';
 import 'package:management/resources/app_exports.dart';
-
-import 'controller/add_video_controller.dart';
 
 class AddVideo extends StatelessWidget{
   @override
@@ -22,22 +15,14 @@ class AddVideo extends StatelessWidget{
     TextEditingController video_thumbnail = TextEditingController();
     TextEditingController live = TextEditingController();
     TextEditingController channel_logo = TextEditingController();
-    List<TextEditingController> god =
-    List.generate(7, (i) => TextEditingController());
-    List<Widget> _godTextFields = List.generate(
-      7,
-          (index) => addCustomTextField(god[index], "God $index"),
-    );
+    TextEditingController god = TextEditingController();
     return Scaffold(
         body: SingleChildScrollView(
           child: Column(
             children: [
 
               addCustomTextField(channel_logo, 'Channel Logo(link)'),
-              ExpandablePanel(
-                  header: redButton("Add God"),
-                  collapsed: const SizedBox(),
-                  expanded: Column(children: _godTextFields)),
+             addCustomTextField(god, 'Add god name comma seprated'),
               addCustomTextField(language, 'Language number'),
               addCustomTextField(puja_key, 'Puja Key'),
               addCustomTextField(puja_name, 'Puja Name'),
@@ -52,26 +37,24 @@ class AddVideo extends StatelessWidget{
                     Get.defaultDialog(
                         middleText: "Are you sure you want to add ${tittle.text} as New video",
                         onConfirm: (){
-                          List<String> gods = [];
-                          god.forEach((element) {
-                            gods.add(element.text);
-                          });
+                          List<String> gods = god.text.split(',');                          
                           if(language.text.isEmpty || puja_name.text.isEmpty || puja_key.text.isEmpty ||video_thumbnail.text.isEmpty || tittle.text.isEmpty || video_id.text.isEmpty){
                             Get.showSnackbar(const GetSnackBar(titleText: Text(''),messageText: Text('Please fill are fields properly'),duration: Duration(seconds: 2),));
                           }
                           else{
 
-                            live.text.trim()=="true"?FirebaseFirestore.instance.doc('/puja_purohit_tv/live_aarti/${video_id.text.trim()}').set({
+                            live.text.trim()=="true"?FirebaseFirestore.instance.doc('PujaPurohitFiles/puja_purohit_tv/live_aarti/${video_id.text.trim()}').set({
                               'language' : int. parse(language.text.trim()) ,
                               "channel_logo":channel_logo.text.trim(),
-                              "god":FieldValue.arrayUnion(gods),
+                              "god":gods[0],
                               'puja_key' : puja_key.text.trim(),
                               'puja_name' : puja_name.text.trim(),
                               'tittle' :tittle.text.trim(),
                               'video_id' :video_id.text.trim(),
                               'video_thumbnail':video_thumbnail.text.trim(),
-                              "live":live.text.trim(),
+                              "live":live.text.trim()=="false"?false:true,
                               "timetamp":FieldValue.serverTimestamp(),
+                              "favourite":350,
                             }):null;
 
                             FirebaseFirestore.instance.doc('/PujaPurohitFiles/all_videos/videos/${video_id.text.trim()}').set({
@@ -83,8 +66,9 @@ class AddVideo extends StatelessWidget{
                               'tittle' :tittle.text.trim(),
                               'video_id' :video_id.text.trim(),
                               'video_thumbnail':video_thumbnail.text.trim(),
-                              "live":live.text.trim(),
+                              "live":live.text.trim()=="false"?false:true,
                               "timetamp":FieldValue.serverTimestamp(),
+                              "favourite":350,
                             }).whenComplete(() {
                               Get.back();
                             });
